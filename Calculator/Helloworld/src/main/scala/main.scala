63b74e59bd2efbe64001fd17971d04562979ace0
ExaltedA <aldie1741@gmail.com>
2020-09-14 04:21:29 +0600

    add project

---
+++ Calculator/Helloworld/src/main/scala/main.scala
@@ -0,0 +1,15 @@
+import scala.collection.mutable.Stack
+import scala.io.StdIn.readLine
+/*
+* Author:Yessenturov Aldiyar
+* aldie1741@gmail.com
+* Simple calculator application
+*/
+
+object main extends App {
+//Create a new instance of Core class object
+  val Calculator = new Core()
+  // Start an application using its method Start()
+  Calculator.Start()
+
+}
